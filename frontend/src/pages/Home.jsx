9fcd8290cd6be3cb299f2a9be57e77f783aff066
Jake Lira <llira09@gmail.com>
2022-10-11 15:19:24 -0500

    Updated register user and if user is logged in grab user from local storage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import { Link } from 'react-router-dom'
+import { FaQuestionCircle, FaTicketAlt } from 'react-icons/fa'
 function Home() {
-  return <div>Home</div>
+  return (
+    <>
+      <section className='heading'>
+        <h1>What do you need help with?</h1>
+        <p>Please choose from the option below</p>
+      </section>
+      <Link to='/new-ticket' className='btn btn-reverse btn-block'>
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to='/ticket' className='btn btn-block'>
+        <FaTicketAlt /> View My Tickets
+      </Link>
+    </>
+  )
 }
 
 export default Home
