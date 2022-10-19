7dbbc9a330107c23a6b48502aff406854cf49383
Jake Lira <llira09@gmail.com>
2022-10-19 12:43:44 -0500

    Update UI for ticket page

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -14,9 +14,35 @@ const createTicket = async (ticketData, token) => {
 
     return response.data
 }
+// Get Tickets
+const getTickets = async (token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.get(API_URL, config)
+
+    return response.data
+}
+// Get Ticket
+const getTicket = async (ticketId, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+
+    const response = await axios.get(API_URL + ticketId, config)
+
+    return response.data
+}
 
 const ticketService = {
     createTicket,
+    getTickets,
+    getTicket
 }
 
 export default ticketService
